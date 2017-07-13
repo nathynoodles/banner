308f5d88ba1b7c608b791b7236ee9d389906db6a
Nathan Chick <nathan@Nathans-Air.home>
2017-07-13 21:08:29 +0100

    complete build

--- app/code/Redhatch/Banner/Setup/InstallSchema.php
+++ app/code/Redhatch/Banner/Setup/InstallSchema.php
@@ -23,6 +23,191 @@ class InstallSchema implements \Magento\Framework\Setup\InstallSchemaInterface
         $installer = $setup;
         $installer->startSetup();
 
+        if ( ! $installer->tableExists( $installer->getTable('redhatch_banner_banner') ) ) {
+
+            $table = $installer->getConnection()->newTable(
+                $installer->getTable('redhatch_banner_banner')
+            )->addColumn(
+                'redhatch_banner_banner_id',
+                \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+                null,
+                ['identity' => true, 'nullable' => false, 'primary' => true, 'unsigned' => true,],
+                'Entity ID'
+            )->addColumn(
+                'identifier',
+                \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                255,
+                ['nullable' => false,],
+                'Identifier'
+            )->addColumn(
+                'name',
+                \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                255,
+                ['nullable' => false,],
+                'Name'
+            )->addColumn(
+                'mode',
+                \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                255,
+                ['nullable' => false,],
+                'Mode'
+            )->addColumn(
+                'speed',
+                \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+                255,
+                ['nullable' => false,],
+                'Speed'
+            )->addColumn(
+                'pause',
+                \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+                null,
+                ['nullable' => false,],
+                'Pause'
+            )->addColumn(
+                'random_start',
+                \Magento\Framework\DB\Ddl\Table::TYPE_SMALLINT,
+                null,
+                ['nullable' => false,],
+                'Random Start'
+            )->addColumn(
+                'infinite_loop',
+                \Magento\Framework\DB\Ddl\Table::TYPE_SMALLINT,
+                null,
+                ['nullable' => false,],
+                'Infinite Loop'
+            )->addColumn(
+                'controls',
+                \Magento\Framework\DB\Ddl\Table::TYPE_SMALLINT,
+                null,
+                ['nullable' => false,],
+                'Controls'
+            )->addColumn(
+                'pager',
+                \Magento\Framework\DB\Ddl\Table::TYPE_SMALLINT,
+                null,
+                ['nullable' => false,],
+                'Page'
+            )->addColumn(
+                'creation_time',
+                \Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP,
+                null,
+                ['nullable' => false, 'default' => \Magento\Framework\DB\Ddl\Table::TIMESTAMP_INIT,],
+                'Creation Time'
+            )->addColumn(
+                'update_time',
+                \Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP,
+                null,
+                ['nullable' => false, 'default' => \Magento\Framework\DB\Ddl\Table::TIMESTAMP_INIT_UPDATE,],
+                'Modification Time'
+            )->addColumn(
+                'is_active',
+                \Magento\Framework\DB\Ddl\Table::TYPE_SMALLINT,
+                null,
+                ['nullable' => false, 'default' => '1',],
+                'Is Active'
+            );
+
+            $installer->getConnection()->createTable($table);
+        }
+
+        if ( ! $installer->tableExists( $installer->getTable('redhatch_banner_item') ) ) {
+
+            $table = $installer->getConnection()->newTable(
+                $installer->getTable('redhatch_banner_item')
+            )->addColumn(
+                'redhatch_banner_item_id',
+                \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+                null,
+                ['identity' => true, 'nullable' => false, 'primary' => true, 'unsigned' => true,],
+                'Entity ID'
+            )->addColumn(
+                'banner_id',
+                \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+                null,
+                ['nullable' => false,],
+                'Banner ID'
+            )->addColumn(
+                'name',
+                \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                255,
+                ['nullable' => false,],
+                'Name'
+            )->addColumn(
+                'link',
+                \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                255,
+                ['nullable' => false,],
+                'Link'
+            )->addColumn(
+                'desktop_image',
+                \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                255,
+                ['nullable' => false,],
+                'Desktop'
+            )->addColumn(
+                'tablet_image',
+                \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                255,
+                ['nullable' => false,],
+                'Tablet'
+            )->addColumn(
+                'mobile_image',
+                \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                255,
+                ['nullable' => false,],
+                'Mobile'
+            )->addColumn(
+                'sort_order',
+                \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
+                255,
+                ['nullable' => false,],
+                'Sort Order'
+            )->addColumn(
+                'customer_group',
+                \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                255,
+                ['nullable' => false,],
+                'Customer Group'
+            )->addColumn(
+                'store_ids',
+                \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                255,
+                ['nullable' => false,],
+                'Store ids'
+            )->addColumn(
+                'display_days',
+                \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                255,
+                ['nullable' => false,],
+                'Display Days'
+            )->addColumn(
+                'display_times',
+                \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                255,
+                ['nullable' => false,],
+                'Display  Times'
+            )->addColumn(
+                'creation_time',
+                \Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP,
+                null,
+                ['nullable' => false, 'default' => \Magento\Framework\DB\Ddl\Table::TIMESTAMP_INIT,],
+                'Creation Time'
+            )->addColumn(
+                'update_time',
+                \Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP,
+                null,
+                ['nullable' => false, 'default' => \Magento\Framework\DB\Ddl\Table::TIMESTAMP_INIT_UPDATE,],
+                'Modification Time'
+            )->addColumn(
+                'is_active',
+                \Magento\Framework\DB\Ddl\Table::TYPE_SMALLINT,
+                null,
+                ['nullable' => false, 'default' => '1',],
+                'Is Active'
+            );
+
+            $installer->getConnection()->createTable($table);
+        }
         /*
         $tableName = ':: EDIT ME ::';
         if (!$installer->tableExists($tableName)) {
